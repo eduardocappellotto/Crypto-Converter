f5a34e1729a8b7305f886a718922049a5e1d8b40
eduardocappellotto <51893328+eduardocappellotto@users.noreply.github.com>
2020-02-13 22:04:17 -0300

    Update no html

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+
 const express = require('express');
 const bodyParser = require('body-parser');
 const request = require('request');
@@ -46,6 +47,7 @@ app.post("/" , function( req , res ){
   
 })});
 
-app.listen(3000, function(){ 
+app.listen(process.env.PORT||3000, function(){ 
     console.log("Server is running on port 3000");
 }); 
+
